dc3e5e5774e1c3399b6de50e7a23480c0b7b5108
Nick parrell <nbparrell@gmail.com>
2018-10-20 22:07:11 -0400

    Update Assets/Scripts/GetAccountInfo.cs
    Script will get the account name from the databse and update Name field with the information.

---
+++ Assets/Scripts/GetAccountInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Text;
+using ConnectionManager;
+public class GetAccountInfo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ConnectionsManager CM = new ConnectionsManager();
+		int result = CM.StartClient();
+		Debug.Log(result);
+		string[] playerInfo = CM.getAccoutInformation();
+		for(int i = 0; i<3; i++){
+			Debug.Log("playerInfo[" + i + "]: " + playerInfo[i]);
+		}
+
+		showUserInfo(playerInfo);
+
+	}
+
+	private void showUserInfo(string[] playerInfo){
+		GameObject userNameField = GameObject.FindGameObjectWithTag("profileName");
+		InputField userNameIput = userNameField.GetComponent<InputField>();
+		userNameIput.text = playerInfo[2];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
