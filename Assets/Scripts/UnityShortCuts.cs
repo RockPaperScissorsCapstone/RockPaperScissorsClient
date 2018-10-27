2ae4cbab291dbbb18f5b14f690fdc13df2f5a609
Nick parrell <nbparrell@gmail.com>
2018-10-26 22:03:45 -0400

    New Script File UnityShortCuts
    A new script to hold Unity Short Cut code such as the InputText function and other such function calls.

---
+++ Assets/Scripts/UnityShortCuts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+namespace UnityShortCuts{
+	public class ShortCuts : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+	
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		public string InputValue(string tagName){
+			GameObject inputField = GameObject.FindGameObjectWithTag(tagName);
+			InputField iput = inputField.GetComponent<InputField>();
+			return iput.text.ToString();
+		}
+	}
+}
