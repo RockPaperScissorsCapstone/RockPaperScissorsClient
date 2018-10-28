1e209053301c3ebd752fd23f1aba4747075ff112
Nick parrell <nbparrell@gmail.com>
2018-10-27 21:05:33 -0400

    Update Assets/Scripts/UnityShortCuts.cs
    Updated the UnityShortCuts.cs so that it has more functions that should make navigating the Unity engine easier and hopefully making things more readable.

--- Assets/Scripts/UnityShortCuts.cs
+++ Assets/Scripts/UnityShortCuts.cs
@@ -6,22 +6,48 @@ using UnityEngine.UI;
 using System.IO;
 
 namespace UnityShortCuts{
-	public class ShortCuts : MonoBehaviour {
+	public class ShortCuts {
+		public ShortCuts(){
 
-		// Use this for initialization
-		void Start () {
-		
-		}
-	
-		// Update is called once per frame
-		void Update () {
-		
 		}
 
+		/* 
+			Helper function that uses the tag name associated with a game object of type InputField
+			to find said game object and return the value of the input fields text in a string format. 
+		*/
 		public string InputValue(string tagName){
-			GameObject inputField = GameObject.FindGameObjectWithTag(tagName);
-			InputField iput = inputField.GetComponent<InputField>();
-			return iput.text.ToString();
+			GameObject inputObject = GameObject.FindGameObjectWithTag(tagName);
+			InputField inputObjectComponent = inputObject.GetComponent<InputField>();
+			return inputObjectComponent.text.ToString();
 		}
+
+		public void updateInputValue(string tagName, string param){
+			GameObject inputObject = GameObject.FindGameObjectWithTag(tagName);
+			InputField inputObjectComponent = inputObject.GetComponent<InputField>();
+			inputObjectComponent.text = param;
+		}
+
+		public void updateInputValue(string[] tagName, string[] param){
+			for(int i = 0; i < tagName.Length; i++){
+				GameObject inputObject = GameObject.FindGameObjectWithTag(tagName[i]);
+				InputField inputObjectComponent = inputObject.GetComponent<InputField>();
+				inputObjectComponent.text = param[i];
+			}
+		}
+
+		public void updateTextValue(string tagName, string param){
+			GameObject textObject = GameObject.FindGameObjectWithTag(tagName);
+			Text textObjectComponent = textObject.GetComponent<Text>();
+			textObjectComponent.text = param;
+		}
+
+		public void updateTextValue(string[] tagName, string[] param){
+			for(int i = 0; i < tagName.Length; i++){
+				GameObject textObject = GameObject.FindGameObjectWithTag(tagName[i]);
+				Text textObjectComponent = textObject.GetComponent<Text>();
+				textObjectComponent.text = param[i];
+			}
+		}
+
 	}
 }
