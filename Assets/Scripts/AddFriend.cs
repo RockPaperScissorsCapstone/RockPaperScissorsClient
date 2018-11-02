49f8138cf3ad8c393afb59215539370c5f91e829
Krishna <tuf95300@temple.edu>
2018-11-02 00:03:12 -0400

    AddFriendWorkInProgress

---
+++ Assets/Scripts/AddFriend.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ServerManager;
+using System.IO;
+using UnityShortCuts;
+using Navigator;
+
+public class AddFriend : MonoBehaviour {
+
+    string user_id;
+    ShortCuts usc;
+    string data;
+    UserInfo playerinfo;
+
+    public void AddNewFriend()
+    {
+        string[] param = new string[2];
+        usc = new ShortCuts();
+        
+        data = File.ReadAllText(Application.dataPath + "/MyInfo.json");
+        playerinfo = JsonUtility.FromJson<UserInfo>(data);
+        param[0] = playerinfo.getUserId();
+        Debug.Log(param[0]);
+
+        param[1] = usc.InputValue("usernameFriend");
+        Debug.Log(param[1]);
+
+
+        Debug.Log(param);
+
+    }
+}
