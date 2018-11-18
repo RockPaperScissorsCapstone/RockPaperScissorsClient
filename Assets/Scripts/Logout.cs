b059559111ad759a3bb015a772931b768ccf5c74
Steve Do <gustmd1210@gmail.com>
2018-11-17 22:43:22 -0500

    User can logout by clicking the button on the MainMenu
    doing so will clear the MyInfo.json

---
+++ Assets/Scripts/Logout.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using ServerManager;
+using System;
+using System.IO;
+
+public class Logout : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//the user clicks the logout button on main menu to trigger this
+	public void logout() {
+		ConnectionManager connectionManager = new ConnectionManager();
+		if (connectionManager.StartClient() == 1) { //successful connection to server
+			//get username from stored user data
+			string data = File.ReadAllText(Application.dataPath + "/MyInfo.json");
+			UserInfo playerinfo = JsonUtility.FromJson<UserInfo>(data);
+			string username = playerinfo.getUsername();
+
+			//tell server to logout
+			string response = connectionManager.LogOut(username);
+			Debug.Log(response);
+
+			//clear stored MyInfo.json
+			UserInfo emptyPlayer = new UserInfo();
+			string emptyJSON = JsonUtility.ToJson(emptyPlayer);
+			File.WriteAllText(Application.dataPath + "/MyInfo.json", emptyJSON);
+
+			//navigate back to LoginScreen
+			SceneManager.LoadScene("LoginScreen");
+		} else {
+			Debug.Log("Connection Manager Failed to Start a Client");
+		}
+	}
+}
