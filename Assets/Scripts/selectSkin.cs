d07489be9bc65d6de7dec7c6e91dc87a869c0153
Illya Yefimenko <iyefimenko@bearincorp.com>
2018-12-01 20:39:17 -0500

    created file for skin select script and modified userinfo to include a attribute reference to the skin

---
+++ Assets/Scripts/selectSkin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectSkin : MonoBehaviour {
+
+    Skin selectedSkin;
+
+    public void setSkin(string skintag)
+    {
+        
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
