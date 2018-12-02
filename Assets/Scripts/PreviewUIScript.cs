6db2881047edabdf28b6a59ea3113b6b9345ce23
Dat <tuf70160@temple.edu>
2018-12-01 23:31:03 -0500

    Preview UI, script, and Skin Class
    a UI for preview skins(On game board)
    need to map X button to hide the thing
    and map it to onclick to popup
    
    preview script works with the UI but may need to change to better implimentation soon
    
    class may need to change depending

---
+++ Assets/Scripts/PreviewUIScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewUIScript : MonoBehaviour {
+
+    public gameObject RockPreview;      //Rock Skin Preview Object
+    public gameObject PaperPreview;     //Paper Skin Preview Object
+    public gameObject ScissorsPreview;  //Scissors Skin Preview Object
+
+
+    // Use this for initialization
+    void Start () {
+        //Gameobject Inizalization
+        RockPreview.gameObject.find("RockPreview");             //Finds RockPreview Object and initalize
+        PaperPreview.gameObject.find("PaperPreview");           //Finds PaperPreview Object and initalize
+        ScissorsPreview.gameObject.find("ScissorsPreview");     //Finds ScissorsPreview and initalize
+
+    }
+
+    public void setRockPreviewSkin(gameObject Skin) //Sets the Rock sprite to gameobject's  
+    {
+        RockPreview.sprite = Skin.getComponent<image>();
+    }
+    public void setPaperPreviewSkin(gameObject Skin)//Sets The Paper sprite to gameobjects
+    {
+
+        PaperPreview.sprite = Skin.getComponent<image>();
+    }
+    public void setScissorsPreviewSkin(gameObject Skin)//Sets the Scissors Sprite to the gameobject's
+    {
+
+        ScissorsPreview.sprite = Skin.getComponent<image>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SkinObjectClass.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This is the Object class for a skin
+//ALL VARIABLES
+public string SkinName;   //Name of Skin
+public string SkinID;     //ID of skin
+public int SkinPrice;     //price of skin via currency
+public bool Owned;        // If SKin is owned or not
+public Image RockSkin;    //The image of the ROCK skin
+public Image PaperSKin;   //The image of the PAPER skin
+public Image ScissorsSkin;//The image of the SCISSORS skin
+
+public class SkinObjectClass : MonoBehaviour {
+
+
+    //ALL initalizing calls FUNCTIONS
+    public SetSkinName(string name)                         //Sets The skin object's Name
+    {
+        SkinName = name;
+    }
+
+    public SetSkinID(string name)
+    {
+        SkinID = name;                                      //Sets SKINID to the name
+    }
+    public SetSkinPrice(int priceofskin)                    //Sets price
+    {
+        SkinPrice = priceofskin;
+    }
+
+    public SetRockSkin(gameObject RockObj)                  //Sets Rock Skin from a Object's Image
+    {
+        RockSkin = RockObj.getComponent<image>();
+
+    }
+
+    public SetPaperSkin(gameObject PaperObj)                //Sets Paper Skin from a Object's image
+    {
+        PaperSkin = PaperObj.getComponent<image>();
+    }
+
+    public SetScissorsSkin(gameObject ScissorsObj)          //Sets Scissors skin from a Object's image
+    {
+        ScissorsSkin = ScissorsObj.getComponent<image>();
+    }
+
+   
+
+
+
+    //ALL RETURN/GET FUNCTIONS
+
+    public getSkinName()
+    {
+        return SkinName;
+    }
+
+    public getSkinID()
+    {
+        return SkinID;
+    }
+
+    public getSkinPrice()
+    {
+        return SkinPrice;
+    }
+
+    public getRockSkin()                                    //Returns Rock Skin
+    {
+        return RockSkin;
+    }
+
+    public setPaperSkin()                                   //Returns Paper Skin
+    {
+        return PaperSkin;
+
+    } 
+
+    public setScissorsSkin()                                //Returns Scissors Skin
+    {
+        return PaperSkin;
+    }
+
+   
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
