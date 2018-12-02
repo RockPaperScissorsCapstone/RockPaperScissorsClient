47eec70782a1eb867c363c2d4661a90b032543b0
Illya Yefimenko <iyefimenko@bearincorp.com>
2018-12-01 20:46:08 -0500

    wrote code that places the skin tag into the player info

--- Assets/Scripts/selectSkin.cs
+++ Assets/Scripts/selectSkin.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class selectSkin : MonoBehaviour {
 
-    Skin selectedSkin;
-
     public void setSkin(string skintag)
     {
-        
-
+        string data = File.ReadAllText(Application.dataPath + "/MyInfo.json");
+        UserInfo playerinfo = JsonUtility.FromJson<UserInfo>(data);
+        playerinfo.setSkinTag(skintag);
+        data = JsonUtility.ToJson(playerinfo);
+        StreamWriter sw = File.CreateText(Application.dataPath + "/MyInfo.json");
+        sw.Close();
+        File.WriteAllText(Application.dataPath + "/MyInfo.json", data);
     }
 
 	// Use this for initialization
